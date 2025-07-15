088c69b814b8a631c7d6f6d70c978aa744343614
illuciaz23 <104995560+illuciaz23@users.noreply.github.com>
2025-07-15 20:44:44 +0800

    Main dev (#39)
    * 修复me高级终端bug
    
    * (jei)为一些材料的来源配方增加了关联物品查询；
    (jei)为具有电压等级的配方增加了颜色边框
    
    * 转子举高高

--- src/main/java/io/github/cpearl0/ctnhcore/client/renderer/TurbineRotorRender.java
+++ src/main/java/io/github/cpearl0/ctnhcore/client/renderer/TurbineRotorRender.java
@@ -39,13 +39,88 @@ public class TurbineRotorRender implements IRenderer {
     @Getter
     private static final TurbineRotorModel model = new TurbineRotorModel();
 
+    float FPLH_ROT_X = -40f;
+    float FPLH_ROT_Y = 0f;
+    float FPLH_ROT_Z = 0f;
+    float FPLH_TRANSLATE_X = 0f;
+    float FPLH_TRANSLATE_Y = 6.0f;
+    float FPLH_TRANSLATE_Z = 0f;
+
+    // FIRST_PERSON_RIGHT_HAND
+    float FPRH_ROT_X = -40f;
+    float FPRH_ROT_Y = 0f;
+    float FPRH_ROT_Z = 0f;
+    float FPRH_TRANSLATE_X = -0.5f;
+    float FPRH_TRANSLATE_Y = 6.0f;
+    float FPRH_TRANSLATE_Z = 0f;
+
+    float FP_SCALE = 8.0f;
+    // THIRD_PERSON_LEFT_HAND
+    float TPLH_ROT_X = 50f;
+    float TPLH_ROT_Y = 0f;
+    float TPLH_ROT_Z = 0f;
+    float TPLH_TRANSLATE_X = 0.375f;
+    float TPLH_TRANSLATE_Y = -0.3f;
+    float TPLH_TRANSLATE_Z = -0.4f;
+
+    // THIRD_PERSON_RIGHT_HAND
+    float TPRH_ROT_X = 50f;
+    float TPRH_ROT_Y = 0f;
+    float TPRH_ROT_Z = 0f;
+    float TPRH_TRANSLATE_X = -0.375f;
+    float TPRH_TRANSLATE_Y = -0.35f;
+    float TPRH_TRANSLATE_Z = -0.25f;
+
     @Override
     public void renderItem(ItemStack stack, ItemDisplayContext transformType, boolean leftHand, PoseStack poseStack, MultiBufferSource buffer, int combinedLight, int combinedOverlay, BakedModel model) {
         TurbineRotorBlock block = (TurbineRotorBlock)((BlockItem)stack.getItem()).getBlock();
 
         poseStack.pushPose();
-        if(transformType == ItemDisplayContext.GUI) poseStack.scale(0.15f,0.15f,0.15f);
-
+        switch (transformType) {
+            case FIRST_PERSON_LEFT_HAND -> {
+                poseStack.mulPose(Axis.XP.rotationDegrees(FPLH_ROT_X));
+                poseStack.mulPose(Axis.YP.rotationDegrees(FPLH_ROT_Y));
+                poseStack.mulPose(Axis.ZP.rotationDegrees(FPLH_ROT_Z));
+                poseStack.translate(
+                        FPLH_TRANSLATE_X,
+                        FPLH_TRANSLATE_Y,
+                        FPLH_TRANSLATE_Z
+                );
+                poseStack.scale(FP_SCALE,FP_SCALE,FP_SCALE);
+            }
+            case FIRST_PERSON_RIGHT_HAND -> {
+                poseStack.mulPose(Axis.XP.rotationDegrees(FPRH_ROT_X));
+                poseStack.mulPose(Axis.YP.rotationDegrees(FPRH_ROT_Y));
+                poseStack.mulPose(Axis.ZP.rotationDegrees(FPRH_ROT_Z));
+                poseStack.translate(
+                        FPRH_TRANSLATE_X,
+                        FPRH_TRANSLATE_Y,
+                        FPRH_TRANSLATE_Z
+                );
+                poseStack.scale(FP_SCALE,FP_SCALE,FP_SCALE);
+            }
+            case THIRD_PERSON_LEFT_HAND -> {
+                poseStack.mulPose(Axis.XP.rotationDegrees(TPLH_ROT_X));
+                poseStack.mulPose(Axis.YP.rotationDegrees(TPLH_ROT_Y));
+                poseStack.mulPose(Axis.ZP.rotationDegrees(TPLH_ROT_Z));
+                poseStack.translate(
+                        TPLH_TRANSLATE_X,
+                        TPLH_TRANSLATE_Y,
+                        TPLH_TRANSLATE_Z
+                );
+            }
+            case THIRD_PERSON_RIGHT_HAND -> {
+                poseStack.mulPose(Axis.XP.rotationDegrees(TPRH_ROT_X));
+                poseStack.mulPose(Axis.YP.rotationDegrees(TPRH_ROT_Y));
+                poseStack.mulPose(Axis.ZP.rotationDegrees(TPRH_ROT_Z));
+                poseStack.translate(
+                        TPRH_TRANSLATE_X,
+                        TPRH_TRANSLATE_Y,
+                        TPRH_TRANSLATE_Z
+                );
+            }
+            case GUI -> poseStack.scale(0.15f,0.15f,0.15f);
+        }
         poseStack.mulPose(Axis.XP.rotationDegrees(40));
         poseStack.mulPose(Axis.YP.rotationDegrees(RenderUtils.getTime()));  //转子旋转, YP为模型的正面
 
