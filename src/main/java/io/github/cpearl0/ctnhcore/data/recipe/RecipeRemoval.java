c9dc9acf8b01ffad1040ebf541549afdca250255
moguang <184676648@qq.com>
2025-08-21 00:29:33 +0800

    modules

--- src/main/java/io/github/cpearl0/ctnhcore/data/recipe/RecipeRemoval.java
+++ src/main/java/io/github/cpearl0/ctnhcore/data/recipe/RecipeRemoval.java
@@ -9,14 +9,14 @@ public class RecipeRemoval {
     }
 
     public static void ctnhRemovals(Consumer<ResourceLocation> registry){
-        registry.accept(new ResourceLocation("gtceu:centrifuge/pgs_separation"));
-        registry.accept(new ResourceLocation("gtceu:electrolyzer/raw_platinum_separation"));
-        registry.accept(new ResourceLocation("gtceu:chemical_reactor/raw_palladium_separation"));
-        registry.accept(new ResourceLocation("gtceu:large_chemical_reactor/inert_metal_mixture_separation"));
-        registry.accept(new ResourceLocation("gtceu:extractor/extract_osmium_tetroxide_dust"));
-        registry.accept(new ResourceLocation("gtceu:centrifuge/pgs_separation"));
-        registry.accept(new ResourceLocation("gtceu:centrifuge/pgs_separation"));
-        registry.accept(new ResourceLocation("gtceu:centrifuge/pgs_separation"));
+        registry.accept(ResourceLocation.parse("gtceu:centrifuge/pgs_separation"));
+        registry.accept(ResourceLocation.parse("gtceu:electrolyzer/raw_platinum_separation"));
+        registry.accept(ResourceLocation.parse("gtceu:chemical_reactor/raw_palladium_separation"));
+        registry.accept(ResourceLocation.parse("gtceu:large_chemical_reactor/inert_metal_mixture_separation"));
+        registry.accept(ResourceLocation.parse("gtceu:extractor/extract_osmium_tetroxide_dust"));
+        registry.accept(ResourceLocation.parse("gtceu:centrifuge/pgs_separation"));
+        registry.accept(ResourceLocation.parse("gtceu:centrifuge/pgs_separation"));
+        registry.accept(ResourceLocation.parse("gtceu:centrifuge/pgs_separation"));
 
     }
 }
