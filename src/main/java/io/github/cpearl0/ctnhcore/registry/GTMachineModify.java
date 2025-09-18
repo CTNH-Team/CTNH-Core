2fbcad7772293c1a8d72f43bf4460e19e740ce73
lucky_block <1774432258@qq.com>
2025-09-18 13:48:22 +0800

    大机器批处理

--- src/main/java/io/github/cpearl0/ctnhcore/registry/GTMachineModify.java
+++ src/main/java/io/github/cpearl0/ctnhcore/registry/GTMachineModify.java
@@ -1,17 +1,20 @@
 package io.github.cpearl0.ctnhcore.registry;
 
+import com.gregtechceu.gtceu.api.machine.MachineDefinition;
 import com.gregtechceu.gtceu.api.machine.MetaMachine;
 import com.gregtechceu.gtceu.api.machine.feature.multiblock.IMultiController;
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
 import com.gregtechceu.gtceu.api.recipe.GTRecipeType;
 import com.gregtechceu.gtceu.api.recipe.OverclockingLogic;
 import com.gregtechceu.gtceu.api.recipe.modifier.ModifierFunction;
+import com.gregtechceu.gtceu.api.recipe.modifier.RecipeModifier;
 import com.gregtechceu.gtceu.api.recipe.modifier.RecipeModifierList;
 import com.gregtechceu.gtceu.common.data.GTRecipeModifiers;
 import com.gregtechceu.gtceu.common.data.machines.GCYMMachines;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,76 +24,40 @@ public class GTMachineModify {
 
     public static BiConsumer<ItemStack, List<Component>> REDUCTION_INFO = (itemStack, list) -> list.add(Component.translatable("ctnh.gcym.reduction").withStyle(ChatFormatting.GREEN));
     public static void init() {
-        GCYMMachines.LARGE_ARC_SMELTER.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_ARC_SMELTER.setTooltipBuilder(GCYMMachines.LARGE_ARC_SMELTER.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_ASSEMBLER.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_ASSEMBLER.setTooltipBuilder(GCYMMachines.LARGE_ASSEMBLER.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_AUTOCLAVE.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_AUTOCLAVE.setTooltipBuilder(GCYMMachines.LARGE_AUTOCLAVE.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_BREWER.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_BREWER.setTooltipBuilder(GCYMMachines.LARGE_BREWER.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_CENTRIFUGE.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_CENTRIFUGE.setTooltipBuilder(GCYMMachines.LARGE_CENTRIFUGE.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_CHEMICAL_BATH.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_CHEMICAL_BATH.setTooltipBuilder(GCYMMachines.LARGE_CHEMICAL_BATH.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_CUTTER.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_CUTTER.setTooltipBuilder(GCYMMachines.LARGE_CUTTER.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_CIRCUIT_ASSEMBLER.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_CIRCUIT_ASSEMBLER.setTooltipBuilder(GCYMMachines.LARGE_CIRCUIT_ASSEMBLER.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_DISTILLERY.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_DISTILLERY.setTooltipBuilder(GCYMMachines.LARGE_DISTILLERY.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_ELECTROLYZER.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_ELECTROLYZER.setTooltipBuilder(GCYMMachines.LARGE_ELECTROLYZER.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_ELECTROMAGNET.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_ELECTROMAGNET.setTooltipBuilder(GCYMMachines.LARGE_ELECTROMAGNET.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_EXTRACTOR.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_EXTRACTOR.setTooltipBuilder(GCYMMachines.LARGE_EXTRACTOR.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_EXTRUDER.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_EXTRUDER.setTooltipBuilder(GCYMMachines.LARGE_EXTRUDER.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_ENGRAVING_LASER.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_ENGRAVING_LASER.setTooltipBuilder(GCYMMachines.LARGE_ENGRAVING_LASER.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_MACERATION_TOWER.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_MACERATION_TOWER.setTooltipBuilder(GCYMMachines.LARGE_MACERATION_TOWER.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_MIXER.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_MIXER.setTooltipBuilder(GCYMMachines.LARGE_MIXER.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_MATERIAL_PRESS.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_MATERIAL_PRESS.setTooltipBuilder(GCYMMachines.LARGE_MATERIAL_PRESS.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_PACKER.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_PACKER.setTooltipBuilder(GCYMMachines.LARGE_PACKER.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_SOLIDIFIER.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_SOLIDIFIER.setTooltipBuilder(GCYMMachines.LARGE_SOLIDIFIER.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_SIFTING_FUNNEL.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_SIFTING_FUNNEL.setTooltipBuilder(GCYMMachines.LARGE_SIFTING_FUNNEL.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.LARGE_WIREMILL.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.LARGE_WIREMILL.setTooltipBuilder(GCYMMachines.LARGE_WIREMILL.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.MEGA_BLAST_FURNACE.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.MEGA_BLAST_FURNACE.setTooltipBuilder(GCYMMachines.MEGA_BLAST_FURNACE.getTooltipBuilder().andThen(REDUCTION_INFO));
-        GCYMMachines.MEGA_VACUUM_FREEZER.setRecipeModifier(new RecipeModifierList(CTNHRecipeModifiers.GCYM_REDUCTION,GTRecipeModifiers.PARALLEL_HATCH,
-                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK));
-        GCYMMachines.MEGA_VACUUM_FREEZER.setTooltipBuilder(GCYMMachines.MEGA_VACUUM_FREEZER.getTooltipBuilder().andThen(REDUCTION_INFO));
-
+        List<MachineDefinition> gcymMachinesToModify = Arrays.asList(
+                GCYMMachines.LARGE_ARC_SMELTER,
+                GCYMMachines.LARGE_AUTOCLAVE,
+                GCYMMachines.LARGE_BREWER,
+                GCYMMachines.LARGE_CENTRIFUGE,
+                GCYMMachines.LARGE_CHEMICAL_BATH,
+                GCYMMachines.LARGE_CUTTER,
+                GCYMMachines.LARGE_CIRCUIT_ASSEMBLER,
+                GCYMMachines.LARGE_DISTILLERY,
+                GCYMMachines.LARGE_ELECTROLYZER,
+                GCYMMachines.LARGE_ELECTROMAGNET,
+                GCYMMachines.LARGE_EXTRACTOR,
+                GCYMMachines.LARGE_EXTRUDER,
+                GCYMMachines.LARGE_ENGRAVING_LASER,
+                GCYMMachines.LARGE_MACERATION_TOWER,
+                GCYMMachines.LARGE_MIXER,
+                GCYMMachines.LARGE_MATERIAL_PRESS,
+                GCYMMachines.LARGE_PACKER,
+                GCYMMachines.LARGE_SOLIDIFIER,
+                GCYMMachines.LARGE_SIFTING_FUNNEL,
+                GCYMMachines.LARGE_WIREMILL,
+                GCYMMachines.MEGA_BLAST_FURNACE,
+                GCYMMachines.MEGA_VACUUM_FREEZER
+        );
+        RecipeModifierList commonModifier = new RecipeModifierList(
+                CTNHRecipeModifiers.GCYM_REDUCTION,
+                GTRecipeModifiers.PARALLEL_HATCH,
+                GTRecipeModifiers.OC_NON_PERFECT_SUBTICK,
+                GTRecipeModifiers.BATCH_MODE
+        );
+        for (MachineDefinition machine : gcymMachinesToModify) {
+            machine.setRecipeModifier(commonModifier);
+            machine.setTooltipBuilder(machine.getTooltipBuilder().andThen(REDUCTION_INFO));
+        }
         modifyGTAssembly();
     }
     private static void modifyGTAssembly() {
@@ -99,19 +66,27 @@ public class GTMachineModify {
         lASBRecipeTypes.add(CTNHRecipeTypes.PRECISION_ASSEMBLY_RECIPES);
         lASB.setRecipeTypes(lASBRecipeTypes.toArray(GTRecipeType[]::new));
         lASB.setTooltipBuilder(lASB.getTooltipBuilder().andThen((itemStack, components) -> {
+            components.add(Component.translatable("ctnh.gcym.reduction").withStyle(ChatFormatting.GREEN));
             components.add(Component.translatable("ctnh.multiblock.precision_assembly.tooltip.0"));
             components.add(Component.translatable("ctnh.multiblock.precision_assembly.tooltip.1"));
         }
         ));
-        lASB.setRecipeModifier(GTMachineModify::assemblyRecipeModifier);
+
+        lASB.setRecipeModifier(
+                new RecipeModifierList(
+                        CTNHRecipeModifiers.GCYM_REDUCTION,
+                        GTMachineModify::assemblyRecipeModifier,
+                        GTRecipeModifiers.BATCH_MODE
+                )
+        );
     }
     private static ModifierFunction assemblyRecipeModifier(MetaMachine machine, GTRecipe gtRecipe) {
         if (gtRecipe.recipeType == CTNHRecipeTypes.PRECISION_ASSEMBLY_RECIPES) {
-            return  GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.NON_PERFECT_OVERCLOCK).getModifier(machine, gtRecipe);
+            return  GTRecipeModifiers.OC_NON_PERFECT_SUBTICK.getModifier(machine, gtRecipe);
         } else {
             return new RecipeModifierList(
                     GTRecipeModifiers.PARALLEL_HATCH,
-                    GTRecipeModifiers.ELECTRIC_OVERCLOCK.apply(OverclockingLogic.NON_PERFECT_OVERCLOCK)
+                    GTRecipeModifiers.OC_NON_PERFECT_SUBTICK
             ).getModifier(machine, gtRecipe);
         }
     }
