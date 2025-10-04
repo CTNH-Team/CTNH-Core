586c025f2f475061c7b6ea01b37c1604f603a5d1
lucky_block <1774432258@qq.com>
2025-10-04 19:58:20 +0800

    放置样板供应器时自动调整附近小机器的IO

--- src/main/java/io/github/cpearl0/ctnhcore/event/ForgeEventHandler.java
+++ src/main/java/io/github/cpearl0/ctnhcore/event/ForgeEventHandler.java
@@ -1,5 +1,8 @@
 package io.github.cpearl0.ctnhcore.event;
 
+import appeng.block.crafting.PatternProviderBlock;
+import com.gregtechceu.gtceu.api.blockentity.MetaMachineBlockEntity;
+import com.gregtechceu.gtceu.api.machine.SimpleTieredMachine;
 import earth.terrarium.adastra.api.systems.OxygenApi;
 import io.github.cpearl0.ctnhcore.CTNHCore;
 import io.github.cpearl0.ctnhcore.common.enchantment.VacuumSealEnchantment;
@@ -7,15 +10,23 @@ import io.github.cpearl0.ctnhcore.legendary.UnderfloorHeatingSystemTempModifier;
 import io.github.cpearl0.ctnhcore.common.machine.multiblock.generator.WindPowerArrayMachine;
 import io.github.cpearl0.ctnhcore.registry.adventure.CTNHEnchantments;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.tags.FluidTags;
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.entity.ai.attributes.AttributeInstance;
 import net.minecraft.world.entity.ai.attributes.AttributeModifier;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent;
 import net.minecraftforge.event.entity.living.LivingEvent;
+import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -51,6 +62,28 @@ public class ForgeEventHandler {
                     player.setAirSupply(Math.min(player.getMaxAirSupply(), player.getAirSupply() + 4 * 10));
                 }
             });
+        }
+    }
+
+    @SubscribeEvent
+    public static void onBlockPlaced(BlockEvent.EntityPlaceEvent event) {
+        if (!(event.getEntity() instanceof Player player)) return;
+
+        if(event.getPlacedBlock().getBlock() instanceof PatternProviderBlock)
+        {
+            BlockPos placedPos = event.getPos();
+            for (Direction dir : Direction.values()) {
+                BlockPos neighborPos = placedPos.relative(dir);
+                BlockEntity be = event.getLevel().getBlockEntity(neighborPos);
+                if(be instanceof MetaMachineBlockEntity metaMachineBlockEntity
+                        && metaMachineBlockEntity.getMetaMachine() instanceof SimpleTieredMachine tieredMachine)
+                {
+                    tieredMachine.setAutoOutputFluids(true);
+                    tieredMachine.setAutoOutputItems(true);
+                    tieredMachine.setOutputFacingFluids(dir.getOpposite());
+                    tieredMachine.setOutputFacingItems(dir.getOpposite());
+                }
+            }
         }
     }
 }
