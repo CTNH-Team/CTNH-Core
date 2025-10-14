53a5a08f4bb666a0c753f6ae40d08bf11a1ca9bf
Li Silin <2667023708@qq.com>
2025-10-14 16:26:30 +0800

    拉取new gradle分支

--- src/main/java/io/github/cpearl0/ctnhcore/common/machine/multiblock/electric/WideParticleAccelerator.java
+++ src/main/java/io/github/cpearl0/ctnhcore/common/machine/multiblock/electric/WideParticleAccelerator.java
@@ -209,7 +209,6 @@ public class WideParticleAccelerator extends WorkableElectricMultiblockMachine i
     //初始化
     @Override
     public boolean beforeWorking(@Nullable GTRecipe recipe) {
-
         return super.beforeWorking(recipe);
     }
     @Override
