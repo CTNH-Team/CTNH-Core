cc02691325e915ba2b811ddbf10a9485c18d521b
ShuaiDong1231 <2424908701@qq.com>
2025-10-19 23:34:23 +0800

    增加异步并行控制舱配方

--- src/main/java/io/github/cpearl0/ctnhcore/data/recipe/AssemblyLineRecipes.java
+++ src/main/java/io/github/cpearl0/ctnhcore/data/recipe/AssemblyLineRecipes.java
@@ -1,6 +1,14 @@
 package io.github.cpearl0.ctnhcore.data.recipe;
 
+import com.gregtechceu.gtceu.common.data.GTBlocks;
+import com.gregtechceu.gtceu.common.data.GTItems;
+import com.gregtechceu.gtceu.common.data.GTMachines;
+import com.gregtechceu.gtceu.common.data.GTMaterials;
+import com.gregtechceu.gtceu.data.recipe.CustomTags;
+import io.github.cpearl0.ctnhcore.registry.CTNHMachines;
+import io.github.cpearl0.ctnhcore.registry.CTNHMaterials;
 import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraftforge.fluids.FluidStack;
 
 import java.util.function.Consumer;
 
@@ -10,23 +18,28 @@ import static com.gregtechceu.gtceu.api.data.tag.TagPrefix.wireGtSingle;
 import static com.gregtechceu.gtceu.common.data.GTItems.*;
 import static com.gregtechceu.gtceu.common.data.GTMaterials.*;
 import static com.gregtechceu.gtceu.common.data.GTRecipeTypes.ASSEMBLY_LINE_RECIPES;
+import static com.gregtechceu.gtceu.common.data.machines.GCYMMachines.PARALLEL_HATCH;
+import static com.moguang.ctnhbio.data.materials.OrganicMaterials.Unstable_Compound;
 import static io.github.cpearl0.ctnhcore.registry.CTNHItems.ADVANCED_RAM_CHIP;
 
 public class AssemblyLineRecipes {
     public static void init(Consumer<FinishedRecipe> provider) {
-//        ASSEMBLY_LINE_RECIPES.recipeBuilder("crystal_mainframe_uv_aram")
-//                .inputItems(frameGt, HSSE, 2)
-//                .inputItems(CRYSTAL_COMPUTER_ZPM, 2)
-//                .inputItems(ADVANCED_RAM_CHIP, 8)
-//                .inputItems(HIGH_POWER_INTEGRATED_CIRCUIT, 2)
-//                .inputItems(wireGtSingle, NiobiumTitanium, 8)
-//                .inputItems(ADVANCED_SMD_INDUCTOR, 8)
-//                .inputItems(ADVANCED_SMD_CAPACITOR, 16)
-//                .inputItems(ADVANCED_SMD_DIODE, 8)
-//                .inputFluids(SolderingAlloy.getFluid(L * 10))
-//                .outputItems(CRYSTAL_MAINFRAME_UV)
-//                .stationResearch(b -> b.researchStack(CRYSTAL_COMPUTER_ZPM.asStack()).CWUt(16))
-//                .EUt(VA[LuV]).duration(600)
-//                .save(provider);
+        ASSEMBLY_LINE_RECIPES.recipeBuilder("async_thread_hatch_luv")
+                .inputItems(GTMachines.HULL[LuV].asStack(),1)
+                .inputItems(GTMachines.QUANTUM_CHEST[LuV].asStack(),4)
+                .inputItems(GTMachines.QUANTUM_TANK[LuV].asStack(),4)
+                .inputItems(CustomTags.UV_CIRCUITS, 4)
+                .inputItems(ENERGY_LAPOTRONIC_ORB_CLUSTER,4)
+                .inputItems(COVER_MACHINE_CONTROLLER,64)
+                .inputItems(COVER_ACTIVITY_DETECTOR_ADVANCED,64)
+                .inputItems(PROGRAMMED_CIRCUIT,64)
+                .inputFluids(new FluidStack(CTNHMaterials.Cerrobase140.getFluid(), 8000))
+                .inputFluids(new FluidStack(GTMaterials.HSSS.getFluid(), 8000))
+                .inputFluids(new FluidStack(GTMaterials.HSSG.getFluid(), 8000))
+                .inputFluids(new FluidStack(GTMaterials.HSSE.getFluid(), 8000))
+                .outputItems(CTNHMachines.ASYNC_THREAD_HATCH[LuV])
+                .stationResearch(b -> b.researchStack(PARALLEL_HATCH[LuV].asStack()).CWUt(8))
+                .EUt(VA[LuV]).duration(1200)
+                .save(provider);
     }
 }
