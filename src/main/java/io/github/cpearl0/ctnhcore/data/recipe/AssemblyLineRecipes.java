3d19b896832289edd11e37469feeaa5cbd796192
ShuaiDong1231 <2424908701@qq.com>
2025-10-20 00:22:34 +0800

    增加数控冶炼炉的配方

--- src/main/java/io/github/cpearl0/ctnhcore/data/recipe/AssemblyLineRecipes.java
+++ src/main/java/io/github/cpearl0/ctnhcore/data/recipe/AssemblyLineRecipes.java
@@ -1,5 +1,6 @@
 package io.github.cpearl0.ctnhcore.data.recipe;
 
+import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
 import com.gregtechceu.gtceu.common.data.GTBlocks;
 import com.gregtechceu.gtceu.common.data.GTItems;
 import com.gregtechceu.gtceu.common.data.GTMachines;
@@ -7,6 +8,10 @@ import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.gregtechceu.gtceu.data.recipe.CustomTags;
 import io.github.cpearl0.ctnhcore.registry.CTNHMachines;
 import io.github.cpearl0.ctnhcore.registry.CTNHMaterials;
+import io.github.cpearl0.ctnhcore.registry.CTNHMultiblockMachines;
+import io.github.cpearl0.ctnhcore.registry.machines.multiblock.MultiblocksA;
+import io.github.cpearl0.ctnhcore.registry.machines.multiblock.MultiblocksB;
+import io.github.cpearl0.ctnhcore.registry.machines.multiblock.MultiblocksC;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraftforge.fluids.FluidStack;
 
@@ -33,13 +38,28 @@ public class AssemblyLineRecipes {
                 .inputItems(COVER_MACHINE_CONTROLLER,64)
                 .inputItems(COVER_ACTIVITY_DETECTOR_ADVANCED,64)
                 .inputItems(PROGRAMMED_CIRCUIT,64)
-                .inputFluids(new FluidStack(CTNHMaterials.Cerrobase140.getFluid(), 8000))
-                .inputFluids(new FluidStack(GTMaterials.HSSS.getFluid(), 8000))
-                .inputFluids(new FluidStack(GTMaterials.HSSG.getFluid(), 8000))
-                .inputFluids(new FluidStack(GTMaterials.HSSE.getFluid(), 8000))
+                .inputFluids(new FluidStack(CTNHMaterials.Cerrobase140.getFluid(), 144*36))
+                .inputFluids(new FluidStack(GTMaterials.HSSS.getFluid(), 144*36))
+                .inputFluids(new FluidStack(GTMaterials.HSSG.getFluid(), 144*36))
+                .inputFluids(new FluidStack(GTMaterials.HSSE.getFluid(), 144*36))
                 .outputItems(CTNHMachines.ASYNC_THREAD_HATCH[LuV])
                 .stationResearch(b -> b.researchStack(PARALLEL_HATCH[LuV].asStack()).CWUt(8))
                 .EUt(VA[LuV]).duration(1200)
+                .save(provider);
+        ASSEMBLY_LINE_RECIPES.recipeBuilder("cnc_alloy_smelter")
+                .inputItems(GTMachines.HULL[LuV].asStack(),1)
+                .inputItems(GTMachines.ALLOY_SMELTER[LuV].asStack(),4)
+                .inputItems(GTMachines.EXTRACTOR[LuV].asStack(),4)
+                .inputItems(CustomTags.LuV_CIRCUITS, 4)
+                .inputItems(TagPrefix.cableGtOctal,VanadiumGallium,16)
+                .inputItems(TagPrefix.cableGtOctal,NiobiumNitride,16)
+                .inputItems(TagPrefix.cableGtOctal,TungstenSteel,16)
+                .inputItems(TagPrefix.cableGtOctal,Platinum,16)
+                .inputFluids(new FluidStack(TitaniumTungstenCarbide.getFluid(), 144*40))
+                .inputFluids(new FluidStack(TantalumCarbide.getFluid(), 144*40))
+                .outputItems(MultiblocksC.CNC_ALLOY_SMELTER,1)
+                .stationResearch(b -> b.researchStack(GTMachines.ALLOY_SMELTER[LuV].asStack()).CWUt(2))
+                .EUt(VA[LuV]).duration(800)
                 .save(provider);
     }
 }
