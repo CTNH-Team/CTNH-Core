99e1c0387fc30846cfee737b854da66498682373
ShuaiDong1231 <2424908701@qq.com>
2025-10-30 22:11:21 +0800

    增加神经系列配方，敌意观测站配方，两个io仓的配方,模拟室配方

--- src/main/java/io/github/cpearl0/ctnhcore/data/recipe/AssemblyLineRecipes.java
+++ src/main/java/io/github/cpearl0/ctnhcore/data/recipe/AssemblyLineRecipes.java
@@ -1,30 +1,35 @@
 package io.github.cpearl0.ctnhcore.data.recipe;
 
+import com.github.elenterius.biomancy.init.ModItems;
 import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
 import com.gregtechceu.gtceu.common.data.GTBlocks;
 import com.gregtechceu.gtceu.common.data.GTItems;
 import com.gregtechceu.gtceu.common.data.GTMachines;
 import com.gregtechceu.gtceu.common.data.GTMaterials;
 import com.gregtechceu.gtceu.data.recipe.CustomTags;
-import io.github.cpearl0.ctnhcore.registry.CTNHMachines;
-import io.github.cpearl0.ctnhcore.registry.CTNHMaterials;
-import io.github.cpearl0.ctnhcore.registry.CTNHMultiblockMachines;
+import com.moguang.ctnhbio.registry.CBBlocks;
+import com.moguang.ctnhbio.registry.CBMachines;
+import com.moguang.ctnhbio.registry.CBMultiblockMachines;
+import io.github.cpearl0.ctnhcore.registry.*;
 import io.github.cpearl0.ctnhcore.registry.machines.multiblock.MultiblocksA;
 import io.github.cpearl0.ctnhcore.registry.machines.multiblock.MultiblocksB;
 import io.github.cpearl0.ctnhcore.registry.machines.multiblock.MultiblocksC;
 import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.registries.ForgeRegistries;
 
 import java.util.function.Consumer;
 
 import static com.gregtechceu.gtceu.api.GTValues.*;
 import static com.gregtechceu.gtceu.api.data.tag.TagPrefix.frameGt;
 import static com.gregtechceu.gtceu.api.data.tag.TagPrefix.wireGtSingle;
+import static com.gregtechceu.gtceu.common.data.GTBlocks.*;
 import static com.gregtechceu.gtceu.common.data.GTItems.*;
 import static com.gregtechceu.gtceu.common.data.GTMaterials.*;
 import static com.gregtechceu.gtceu.common.data.GTRecipeTypes.ASSEMBLY_LINE_RECIPES;
 import static com.gregtechceu.gtceu.common.data.machines.GCYMMachines.PARALLEL_HATCH;
-import static com.moguang.ctnhbio.data.materials.OrganicMaterials.Unstable_Compound;
+import static com.moguang.ctnhbio.data.materials.OrganicMaterials.*;
 import static io.github.cpearl0.ctnhcore.registry.CTNHItems.ADVANCED_RAM_CHIP;
 
 public class AssemblyLineRecipes {
@@ -60,6 +65,86 @@ public class AssemblyLineRecipes {
                 .outputItems(MultiblocksC.CNC_ALLOY_SMELTER,1)
                 .stationResearch(b -> b.researchStack(GTMachines.ALLOY_SMELTER[LuV].asStack()).CWUt(2))
                 .EUt(VA[LuV]).duration(800)
+                .save(provider);
+        ASSEMBLY_LINE_RECIPES.recipeBuilder("parabiotic_bridge")
+                .inputItems(ModItems.CREATOR_MIX,4)
+                .inputItems(ModItems.LIVING_FLESH,4)
+                .inputItems(CustomTags.LuV_CIRCUITS,4)
+                .inputItems(CTNHMachines.CIRCUIT_BUS[IV],2)
+                .inputItems(GTMachines.ITEM_IMPORT_BUS[ZPM],1)
+                .inputItems(GTMachines.ITEM_EXPORT_BUS[ZPM],1)
+                .inputFluids(new FluidStack(Primordial_Serum.getFluid(), 144*20))
+                .outputItems(CBMachines.PARABIOTIC_BRIDGE,1)
+                .stationResearch(b -> b.researchStack(ModItems.CREATOR_MIX.get().getDefaultInstance()).CWUt(8))
+                .EUt(VA[LuV]).duration(400)
+                .save(provider);
+        ASSEMBLY_LINE_RECIPES.recipeBuilder("consciousness_linker")
+                .inputItems(CBBlocks.NEURAL_NETWORK_CASING,1)
+                .inputItems(CTNHBlocks.SUPER_FREEZE_BLOCK,4)
+                .inputItems(TagPrefix.dust,Promethium,16)
+                .inputItems(CTNHItems.HEAVY_PLATE_T2,16)
+                .inputFluids(new FluidStack(CTNHMaterials.Cerrobase140.getFluid(), 144*10))
+                .inputFluids(new FluidStack(CTNHMaterials.Cryotheum.getFluid(), 144*10))
+                .outputItems(CBBlocks.CONSCIOUSNESS_LINKER,1)
+                .stationResearch(b -> b.researchStack(CTNHBlocks.SUPER_FREEZE_BLOCK.asStack()).CWUt(4))
+                .EUt(VA[LuV]).duration(800)
+                .save(provider);
+        ASSEMBLY_LINE_RECIPES.recipeBuilder("consciousness_controller")
+                .inputItems(CBBlocks.NEURAL_NETWORK_CASING,1)
+                .inputItems(CTNHBlocks.BLAZE_BLAST_FURNACE_CASING,4)
+                .inputItems(TagPrefix.dust,Praseodymium,16)
+                .inputItems(CTNHItems.HEAVY_PLATE_T2,16)
+                .inputFluids(new FluidStack(CTNHMaterials.Cerrobase140.getFluid(), 144*10))
+                .inputFluids(new FluidStack(CTNHMaterials.Pyrotheum.getFluid(), 144*10))
+                .outputItems(CBBlocks.CONSCIOUSNESS_CONTROLLER,1)
+                .stationResearch(b -> b.researchStack(CTNHBlocks.BLAZE_BLAST_FURNACE_CASING.asStack()).CWUt(4))
+                .EUt(VA[LuV]).duration(800)
+                .save(provider);
+        ASSEMBLY_LINE_RECIPES.recipeBuilder("neural_cooling_conduit")
+                .inputItems(CBBlocks.NEURAL_NETWORK_CASING,1)
+                .inputItems(TagPrefix.pipeTinyFluid,Europium,16)
+                .inputItems(TagPrefix.pipeTinyFluid,Europium,16)
+                .inputItems(TagPrefix.pipeTinyFluid,Europium,16)
+                .inputFluids(new FluidStack(CTNHMaterials.Cerrobase140.getFluid(), 144*10))
+                .inputFluids(new FluidStack(Helium.getFluid(), 144*10))
+                .outputItems(CBBlocks.NEURAL_COOLING_CONDUIT,1)
+                .stationResearch(b -> b.researchStack(CTNHBlocks.CASING_POLYBENZIMIDAZOLE_PIPE.asStack()).CWUt(8))
+                .EUt(VA[LuV]).duration(800)
+                .save(provider);
+        ASSEMBLY_LINE_RECIPES.recipeBuilder("sim_chamber")
+                .inputItems(GTMachines.HULL[IV],1)
+                .inputItems(CTNHItems.HEAVY_PLATE_T3,4)
+                .inputItems(TagPrefix.plateDouble,VanadiumGallium,4)
+                .inputItems(TagPrefix.plateDouble,NiobiumTitanium,4)
+                .inputItems(TagPrefix.plateDouble,Tantalum,4)
+                .inputItems(CustomTags.UV_CIRCUITS,8)
+                .inputItems(CBBlocks.CONSCIOUSNESS_SENSOR_GLASS,16)
+                .inputFluids(new FluidStack(SolderingAlloy.getFluid(), 144*5))
+                .inputFluids(new FluidStack(BlueSteel.getFluid(), 144*5))
+                .inputFluids(new FluidStack(Heterogeneous_Compound.getFluid(), 144*5))
+                .outputItems(ForgeRegistries.ITEMS.getValue(ResourceLocation.parse("hostilenetworks:sim_chamber")),1)
+                .stationResearch(b -> b.researchStack(CBBlocks.CONSCIOUSNESS_SENSOR_GLASS.asStack()).CWUt(8))
+                .EUt(VA[LuV]).duration(500)
+                .save(provider);
+        ASSEMBLY_LINE_RECIPES.recipeBuilder("hostile_observer")
+                .inputItems(CTNHItems.HEAVY_PLATE_T3,64)
+                .inputItems(CTNHItems.HEAVY_PLATE_T3,64)
+                .inputItems(CTNHItems.HEAVY_PLATE_T3,64)
+                .inputItems(CTNHItems.HEAVY_PLATE_T3,64)
+                .inputItems(CTNHBlocks.CASING_POLYBENZIMIDAZOLE_PIPE,16)
+                .inputItems(CASING_POLYTETRAFLUOROETHYLENE_PIPE,4)
+                .inputItems(CASING_TITANIUM_PIPE,4)
+                .inputItems(CASING_STEEL_PIPE,4)
+                .inputItems(CustomTags.UV_CIRCUITS,16)
+                .inputItems(CBBlocks.CONSCIOUSNESS_SENSOR_GLASS,16)
+                .inputItems(CBBlocks.NEURAL_NETWORK_CASING,16)
+                .inputItems(ModItems.LIVING_FLESH,16)
+                .inputFluids(new FluidStack(Primordial_Serum.getFluid(), 144*5))
+                .inputFluids(new FluidStack(Heterogeneous_Compound.getFluid(), 144*5))
+                .inputFluids(new FluidStack(Unstable_Compound.getFluid(), 144*5))
+                .outputItems(ForgeRegistries.ITEMS.getValue(ResourceLocation.parse("ctnhbio:hostile_observer")))
+                .stationResearch(b -> b.researchStack(ForgeRegistries.ITEMS.getValue(ResourceLocation.parse("hostilenetworks:sim_chamber")).getDefaultInstance()).CWUt(24))
+                .EUt(VA[ZPM]).duration(1000)
                 .save(provider);
     }
 }
