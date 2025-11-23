3462d7febe22d51b70fb6d6af38500d781874469
Li Silin <2667023708@qq.com>
2025-11-23 23:07:01 +0800

    完善血魔法舱室代码，修改部分变量名

--- src/main/java/io/github/cpearl0/ctnhcore/registry/CTNHGuiTextures.java
+++ src/main/java/io/github/cpearl0/ctnhcore/registry/CTNHGuiTextures.java
@@ -9,6 +9,7 @@ public class CTNHGuiTextures {
     public static final ResourceTexture TEST_BAR_FULL=new ResourceTexture("ctnhcore:textures/gui/texture_test_full.png");
     public static final ResourceTexture COLD_ICON = new ResourceTexture("ctnhcore:textures/gui/c_gui.png");
     public static final ResourceTexture ENERGY_ICON = new ResourceTexture("ctnhcore:textures/gui/energy.png");
+
     public static final ResourceBorderTexture FREEZE_BACKGROUND = new ResourceBorderTexture(
             "ctnhcore:textures/gui/freeze_background.png", 24, 24, 4, 4);
     public CTNHGuiTextures() {
