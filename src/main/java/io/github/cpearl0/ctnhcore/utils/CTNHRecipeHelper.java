7ea696417306b0ac911b78943135ee1229f84f8d
Li Silin <2667023708@qq.com>
2026-01-20 23:20:03 +0800

    完成两个红石舱室和60%的魔力核

--- src/main/java/io/github/cpearl0/ctnhcore/utils/CTNHRecipeHelper.java
+++ src/main/java/io/github/cpearl0/ctnhcore/utils/CTNHRecipeHelper.java
@@ -1,10 +1,16 @@
 package io.github.cpearl0.ctnhcore.utils;
 
+import com.gregtechceu.gtceu.api.machine.trait.NotifiableItemStackHandler;
 import com.gregtechceu.gtceu.api.recipe.GTRecipe;
 import com.gregtechceu.gtceu.api.recipe.RecipeHelper;
 import com.gregtechceu.gtceu.common.data.GTRecipeCapabilities;
 
 import io.netty.util.internal.UnstableApi;
+import it.unimi.dsi.fastutil.ints.IntArrayList;
+import it.unimi.dsi.fastutil.ints.IntList;
+import net.minecraft.world.item.ItemStack;
+import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.ItemHandlerHelper;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
@@ -15,5 +21,56 @@ public class CTNHRecipeHelper extends RecipeHelper {
     public static int getInputCWUt(@NotNull GTRecipe recipe) {
         return recipe.tickInputs.getOrDefault(GTRecipeCapabilities.CWU, new ArrayList<>())
                 .stream().mapToInt(i -> (int) i.content).sum();
+    }
+    public static ItemStack insertItemToOutput(NotifiableItemStackHandler handler, ItemStack stack, boolean simulate) {
+        if (handler == null || stack.isEmpty()) {
+            return stack;
+        }
+        if (!stack.isStackable()) {
+            return insertToEmpty(handler, stack, simulate);
+        }
+
+        IntList emptySlots = new IntArrayList();
+        int slots = handler.getSlots();
+
+        for (int i = 0; i < slots; i++) {
+            ItemStack slotStack = handler.getStackInSlot(i);
+            if (slotStack.isEmpty()) {
+                emptySlots.add(i);
+            } else if (ItemHandlerHelper.canItemStacksStack(stack, slotStack)) {
+                stack = handler.insertItemInternal(i, stack, simulate);
+                if (stack.isEmpty()) {
+                    return ItemStack.EMPTY;
+                }
+            }
+        }
+
+        for (int slot : emptySlots) {
+            stack = handler.insertItemInternal(slot, stack, simulate);
+            if (stack.isEmpty()) {
+                return ItemStack.EMPTY;
+            }
+        }
+        return stack;
+    }
+
+    /**
+     * Only inerts to empty slots. Perfect for not stackable items
+     */
+    public static ItemStack insertToEmpty(NotifiableItemStackHandler handler, ItemStack stack, boolean simulate) {
+        if (handler == null || stack.isEmpty()) {
+            return stack;
+        }
+        int slots = handler.getSlots();
+        for (int i = 0; i < slots; i++) {
+            ItemStack slotStack = handler.getStackInSlot(i);
+            if (slotStack.isEmpty()) {
+                stack = handler.insertItemInternal(i, stack, simulate);
+                if (stack.isEmpty()) {
+                    return ItemStack.EMPTY;
+                }
+            }
+        }
+        return stack;
     }
 }
