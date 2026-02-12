9581a8cb8d07527992831aec84d4ca8ab4b6b2f4
moguang <184676648@qq.com>
2026-02-13 02:33:56 +0800

    暂时回退星辉陨石生成位置的改动

--- src/main/java/io/github/cpearl0/ctnhcore/registry/worldgen/sturcture/CTNHStructures.java
+++ src/main/java/io/github/cpearl0/ctnhcore/registry/worldgen/sturcture/CTNHStructures.java
@@ -1,6 +1,7 @@
 package io.github.cpearl0.ctnhcore.registry.worldgen.sturcture;
 
 import io.github.cpearl0.ctnhcore.CTNHCore;
+import io.github.cpearl0.ctnhcore.registry.worldgen.CTNHBiomes;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
@@ -16,15 +17,11 @@ public class CTNHStructures {
     public static final ResourceKey<Structure> ASTRAL_METEOR = ResourceKey.create(Registries.STRUCTURE, CTNHCore.id("meteorite"));
     public static void bootstrap(BootstapContext<Structure> context) {
         var biomes = context.lookup(Registries.BIOME);
-        var lunarWastelandsKey = ResourceKey.create(
-                Registries.BIOME,
-                ResourceLocation.tryParse("ad_astra:lunar_wastelands")
-        );
         context.register(
                 ASTRAL_METEOR,
                 new AstralMeteorStructure(
                         new Structure.StructureSettings(
-                                HolderSet.direct(biomes.getOrThrow(lunarWastelandsKey)),
+                                HolderSet.direct(biomes.getOrThrow(CTNHBiomes.PLAGUE_WASTELAND)),
                                 Map.of(),
                                 GenerationStep.Decoration.TOP_LAYER_MODIFICATION,
                                 TerrainAdjustment.NONE)));
