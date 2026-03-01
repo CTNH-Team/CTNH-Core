c2f0f42c9a27097f07e79c661046a5d8f9cc67c3
lucky_block <1774432258@qq.com>
2026-03-01 15:03:02 +0800

    fix:禁用aecs emi插件

---
+++ src/main/java/io/github/cpearl0/ctnhcore/mixin/aecs/EmiPluginMixin.java
@@ -0,0 +1,18 @@
+package io.github.cpearl0.ctnhcore.mixin.aecs;
+
+import dev.emi.emi.api.EmiRegistry;
+import io.github.lounode.ae2cs.integration.emi.EmiPlugin;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(value = EmiPlugin.class, remap = false)
+public class EmiPluginMixin {
+    /**
+     * @author
+     * @reason
+     */
+    @Overwrite
+    public void register(EmiRegistry registry) {
+
+    }
+}
