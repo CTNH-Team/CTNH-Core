1f74f61636753b562278cebfbc754fd1639297b0
mmyddd <mmydddd@gmail.com>
2026-05-02 14:26:31 +0800

    feat: 创建GtceuScriptRecipes.java骨架并注册到GTAddon

---
+++ src/main/java/io/github/cpearl0/ctnhcore/data/recipe/GtceuScriptRecipes.java
@@ -0,0 +1,33 @@
+package io.github.cpearl0.ctnhcore.data.recipe;
+
+import io.github.cpearl0.ctnhcore.CTNHCore;
+import io.github.cpearl0.ctnhcore.registry.CTNHItems;
+import io.github.cpearl0.ctnhcore.registry.CTNHRecipeTypes;
+
+import com.gregtechceu.gtceu.api.GTValues;
+import com.gregtechceu.gtceu.api.data.tag.TagPrefix;
+import com.gregtechceu.gtceu.api.machine.multiblock.CleanroomType;
+import com.gregtechceu.gtceu.common.data.*;
+import com.gregtechceu.gtceu.data.recipe.CustomTags;
+import com.gregtechceu.gtceu.data.recipe.VanillaRecipeHelper;
+
+import net.minecraft.data.recipes.FinishedRecipe;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+
+import java.util.function.Consumer;
+
+import static com.gregtechceu.gtceu.api.GTValues.*;
+import static com.gregtechceu.gtceu.api.data.tag.TagPrefix.*;
+import static com.gregtechceu.gtceu.common.data.GTBlocks.*;
+import static com.gregtechceu.gtceu.common.data.GTItems.*;
+import static com.gregtechceu.gtceu.common.data.GTMaterials.*;
+import static com.gregtechceu.gtceu.common.data.GTRecipeTypes.*;
+import static io.github.cpearl0.ctnhcore.registry.CTNHItems.*;
+
+public class GtceuScriptRecipes {
+
+    public static void init(Consumer<FinishedRecipe> provider) {
+        // TODO: Add migrated recipes here
+    }
+}
