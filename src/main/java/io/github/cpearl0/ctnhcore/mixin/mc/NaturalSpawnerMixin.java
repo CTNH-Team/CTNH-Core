e1f29173f0070cee070d4f23610ba5bdc2d630f8
lucky_block <1774432258@qq.com>
2026-06-29 20:09:32 +0800

    修改刷怪优化

--- src/main/java/io/github/cpearl0/ctnhcore/mixin/mc/NaturalSpawnerMixin.java
+++ src/main/java/io/github/cpearl0/ctnhcore/mixin/mc/NaturalSpawnerMixin.java
@@ -3,7 +3,6 @@ package io.github.cpearl0.ctnhcore.mixin.mc;
 import io.github.cpearl0.ctnhcore.CTNHConfig;
 
 import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.NaturalSpawner;
 import net.minecraft.world.level.chunk.LevelChunk;
 
@@ -15,9 +14,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(value = NaturalSpawner.class)
 public class NaturalSpawnerMixin {
 
-    @Inject(method = "spawnCategoryForChunk", at = @At("HEAD"), cancellable = true)
-    private static void sampleChunks(MobCategory category, ServerLevel level, LevelChunk chunk,
-                                     NaturalSpawner.SpawnPredicate filter, NaturalSpawner.AfterSpawnCallback callback,
+    @Inject(method = "spawnForChunk", at = @At("HEAD"), cancellable = true)
+    private static void sampleChunks(ServerLevel level, LevelChunk chunk, NaturalSpawner.SpawnState spawnState,
+                                     boolean spawnFriendlies, boolean spawnMonsters, boolean forcedDespawn,
                                      CallbackInfo ci) {
         int hash = chunk.getPos().x * 31 + chunk.getPos().z;
         long time = level.getGameTime();
