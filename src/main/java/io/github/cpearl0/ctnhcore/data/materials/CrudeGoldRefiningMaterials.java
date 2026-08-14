7bd02cc6c9f390bb00245d5e6a65c978b5617e71
uraniniteresidue <2549847059@qq.com>
2026-08-15 01:10:39 +0800

    金线

--- src/main/java/io/github/cpearl0/ctnhcore/data/materials/CrudeGoldRefiningMaterials.java
+++ src/main/java/io/github/cpearl0/ctnhcore/data/materials/CrudeGoldRefiningMaterials.java
@@ -14,6 +14,13 @@ public class CrudeGoldRefiningMaterials {
     public static Material GOLD_LEACH;
     public static Material COPPER_LEACH;
     public static Material CHLOROAURIC_ACID;
+    public static Material SODIUM_CYANIDE;
+    public static Material PRECIOUS_METAL_CYANO_COMPLEX;
+    public static Material ZINC_CYANIDE_COMPLEX;
+    public static Material CYANIDE_TAILINGS;
+    public static Material GOLD_MUD;
+    public static Material COPPER_CHLORIDE;
+    public static Material SILVER_CHLORIDE;
 
     public static void init() {
         GOLD_ALLOY = REGISTRATE.material(CTNHCore.id("gold_alloy"))
@@ -42,9 +49,53 @@ public class CrudeGoldRefiningMaterials {
                 .buildAndRegister();
         CHLOROAURIC_ACID = REGISTRATE.material(CTNHCore.id("chloroauric_acid"))
                 .cnlang("氯金酸")
-                .formula("AuCl3")
+                .formula("HAuCl4")
                 .liquid()
                 .color(0xBAB02D)
+                .buildAndRegister();
+        SODIUM_CYANIDE = REGISTRATE.material(CTNHCore.id("sodium_cyanide"))
+                .cnlang("氰化钠")
+                .formula("NaCN")
+                .liquid()
+                .color(0xD8D8D8)
+                .buildAndRegister();
+        PRECIOUS_METAL_CYANO_COMPLEX = REGISTRATE.material(CTNHCore.id("precious_metal_cyano_complex"))
+                .cnlang("贵金属氰络合物")
+                .formula("(Na[Au(CN)2])(NaOH)")
+                .liquid()
+                .color(0xC9A227)
+                .buildAndRegister();
+        ZINC_CYANIDE_COMPLEX = REGISTRATE.material(CTNHCore.id("zinc_cyanide_complex"))
+                .cnlang("氰化锌络合物")
+                .formula("(Zn(CN)2)(NaCN)2(NaOH)2")
+                .liquid()
+                .color(0x9EA7AD)
+                .buildAndRegister();
+        CYANIDE_TAILINGS = REGISTRATE.material(CTNHCore.id("cyanide_tailings"))
+                .cnlang("氰化尾渣")
+                .formula("?")
+                .dust()
+                .color(0x6E5B3A)
+                .buildAndRegister();
+        GOLD_MUD = REGISTRATE.material(CTNHCore.id("gold_mud"))
+                .cnlang("金泥")
+                .formula("(Au?)")
+                .dust()
+                .liquid()
+                .color(0xC7A317)
+                .iconSet(METALLIC)
+                .buildAndRegister();
+        COPPER_CHLORIDE = REGISTRATE.material(CTNHCore.id("copper_chloride"))
+                .cnlang("氯化铜")
+                .formula("CuCl2")
+                .dust()
+                .color(0x6FA5C9)
+                .buildAndRegister();
+        SILVER_CHLORIDE = REGISTRATE.material(CTNHCore.id("silver_chloride"))
+                .cnlang("氯化银")
+                .formula("AgCl")
+                .dust()
+                .color(0xD8D8DE)
                 .buildAndRegister();
     }
 }
--- src/main/java/io/github/cpearl0/ctnhcore/data/recipe/chain/GoldChain.java
+++ src/main/java/io/github/cpearl0/ctnhcore/data/recipe/chain/GoldChain.java
@@ -1,10 +1,10 @@
 package io.github.cpearl0.ctnhcore.data.recipe.chain;
 
 import io.github.cpearl0.ctnhcore.CTNHCore;
-import io.github.cpearl0.ctnhcore.data.materials.BauxiteProcessingMaterials;
 
-import com.gregtechceu.gtceu.common.data.GTMaterials;
+import com.gregtechceu.gtceu.api.data.chemical.ChemicalHelper;
 import com.gregtechceu.gtceu.common.data.GTRecipeTypes;
+import com.gregtechceu.gtceu.data.recipe.VanillaRecipeHelper;
 
 import net.minecraft.data.recipes.FinishedRecipe;
 
@@ -13,34 +13,118 @@ import java.util.function.Consumer;
 import static com.gregtechceu.gtceu.api.data.tag.TagPrefix.*;
 import static com.gregtechceu.gtceu.common.data.GTMaterials.*;
 import static io.github.cpearl0.ctnhcore.data.materials.CrudeGoldRefiningMaterials.*;
+import static io.github.cpearl0.ctnhcore.data.materials.PlatinumLineMaterials.FerricSulfate;
+import static io.github.cpearl0.ctnhcore.data.materials.PlatinumLineMaterials.FerrousSulfate;
 import static io.github.cpearl0.ctnhcore.registry.material.CTNHMaterials.PreciousAlloy;
 
 public class GoldChain {
 
     public static void init(Consumer<FinishedRecipe> provider) {
-        // 从 GoldChain.js 迁移
-        // 1. Tier1 gold processing: gold_alloy_dust -> tiny_gold_dust + copper_dust
-        GTRecipeTypes.CENTRIFUGE_RECIPES.recipeBuilder(CTNHCore.id("tier1_gold_processing"))
-                .inputItems(dust, GOLD_ALLOY, 4)
-                .outputItems(dustTiny, Gold, 4)
-                .outputItems(dust, Copper, 3)
-                .EUt(30).duration(500)
+        GTRecipeTypes.CENTRIFUGE_RECIPES.recipeBuilder(CTNHCore.id("precious_alloy_centrifuge"))
+                .inputItems(dust, PreciousAlloy, 4)
+                .outputItems(dust, Gold, 2)
+                .outputItems(dust, Copper, 1)
+                .outputItems(dust, Silver, 1)
+                .EUt(30).duration(100)
                 .save(provider);
 
-        // 2. Tier2 gold processing: gold_leach_dust + hydrogen -> water + copper_dust + tiny_gold_dust
-        GTRecipeTypes.ELECTROLYZER_RECIPES.recipeBuilder(CTNHCore.id("tier2_gold_processing"))
-                .inputItems(dust, GOLD_LEACH, 4)
-                .inputFluids(Hydrogen.getFluid(1000))
-                .outputFluids(Water.getFluid(1000))
-                .outputItems(dust, Copper, 3)
-                .outputItems(dustTiny, Gold, 8)
-                .EUt(30).duration(300)
+        GTRecipeTypes.CENTRIFUGE_RECIPES.recipeBuilder(CTNHCore.id("precious_alloy_centrifuge_tiny"))
+                .inputItems(dust, PreciousAlloy, 1)
+                .outputItems(dustTiny, Gold, 6)
+                .outputItems(dustTiny, Copper, 2)
+                .outputItems(dustTiny, Silver, 1)
+                .EUt(30).duration(100)
                 .save(provider);
 
-        // 3. Tier3 gold processing: copper_leach_dust -> copper_dust + chancedOutput(lead/iron/gallium/nickel/silver)
-        GTRecipeTypes.SIFTER_RECIPES.recipeBuilder(CTNHCore.id("tier3_gold_processing"))
-                .inputItems(dust, COPPER_LEACH, 4)
-                .outputItems(dust, Copper, 3)
+        GTRecipeTypes.CHEMICAL_RECIPES.recipeBuilder(CTNHCore.id("precious_alloy_cyanidation"))
+                .inputItems(dust, PreciousAlloy, 4)
+                .inputFluids(SODIUM_CYANIDE.getFluid(8000))
+                .inputFluids(Oxygen.getFluid(2000))
+                .inputFluids(Water.getFluid(2000))
+                .outputFluids(PRECIOUS_METAL_CYANO_COMPLEX.getFluid(4000))
+                .outputItems(dust, CYANIDE_TAILINGS, 1)
+                .EUt(30).duration(100)
+                .save(provider);
+
+        GTRecipeTypes.CHEMICAL_RECIPES.recipeBuilder(CTNHCore.id("gold_mud_precipitation"))
+                .inputFluids(PRECIOUS_METAL_CYANO_COMPLEX.getFluid(4000))
+                .inputItems(dust, Zinc, 2)
+                .outputFluids(ZINC_CYANIDE_COMPLEX.getFluid(2000))
+                .outputItems(dust, GOLD_MUD, 4)
+                .EUt(30).duration(100)
+                .save(provider);
+
+        GTRecipeTypes.EXTRACTOR_RECIPES.recipeBuilder(CTNHCore.id("gold_mud_melting"))
+                .inputItems(dust, GOLD_MUD, 1)
+                .outputFluids(GOLD_MUD.getFluid(144))
+                .EUt(30).duration(100)
+                .save(provider);
+
+        GTRecipeTypes.CHEMICAL_RECIPES.recipeBuilder(CTNHCore.id("gold_mud_chlorination"))
+                .inputFluids(GOLD_MUD.getFluid(576))
+                .inputFluids(Chlorine.getFluid(3000))
+                .outputFluids(Gold.getFluid(576))
+                .outputItems(dust, COPPER_CHLORIDE, 3)
+                .outputItems(dust, SILVER_CHLORIDE, 2)
+                .EUt(30).duration(100)
+                .save(provider);
+
+        GTRecipeTypes.LARGE_CHEMICAL_RECIPES.recipeBuilder(CTNHCore.id("gold_mud_aqua_regia"))
+                .inputItems(dust, GOLD_MUD, 2)
+                .inputFluids(AquaRegia.getFluid(7000))
+                .outputFluids(CHLOROAURIC_ACID.getFluid(2000))
+                .outputItems(dust, SILVER_CHLORIDE, 3)
+                .outputFluids(NitrogenDioxide.getFluid(7000))
+                .outputFluids(DilutedHydrochloricAcid.getFluid(5000))
+                .EUt(30).duration(100)
+                .save(provider);
+
+        GTRecipeTypes.ELECTROLYZER_RECIPES.recipeBuilder(CTNHCore.id("gold_mud_electrolysis"))
+                .inputItems(dust, GOLD_MUD, 1)
+                .inputItems(plate, Gold, 1)
+                .notConsumableFluid(CHLOROAURIC_ACID.getFluid(1000))
+                .outputItems(dust, Gold, 1)
+                .outputItems(plateDouble, Gold, 1)
+                .EUt(30).duration(100)
+                .save(provider);
+
+        GTRecipeTypes.CHEMICAL_RECIPES.recipeBuilder(CTNHCore.id("gold_from_ferrous_sulfate"))
+                .inputFluids(CHLOROAURIC_ACID.getFluid(2000))
+                .inputFluids(FerrousSulfate.getFluid(3000))
+                .outputItems(dust, Gold, 2)
+                .outputFluids(FerricSulfate.getFluid(3000))
+                .outputFluids(HydrochloricAcid.getFluid(8000))
+                .EUt(30).duration(100)
+                .save(provider);
+
+        GTRecipeTypes.CHEMICAL_RECIPES.recipeBuilder(CTNHCore.id("hydrogen_cyanide_from_methane"))
+                .inputFluids(Methane.getFluid(1000))
+                .inputFluids(Ammonia.getFluid(1000))
+                .outputFluids(HydrogenCyanide.getFluid(1000))
+                .outputFluids(Hydrogen.getFluid(6000))
+                .EUt(30).duration(100)
+                .save(provider);
+
+        GTRecipeTypes.CHEMICAL_RECIPES.recipeBuilder(CTNHCore.id("zinc_cyanide_complex_sulfidation"))
+                .inputFluids(ZINC_CYANIDE_COMPLEX.getFluid(1000))
+                .inputFluids(HydrogenSulfide.getFluid(1000))
+                .outputItems(dust, ZincSulfide, 1)
+                .outputFluids(SODIUM_CYANIDE.getFluid(4000))
+                .EUt(30).duration(100)
+                .save(provider);
+
+        GTRecipeTypes.BLAST_RECIPES.recipeBuilder(CTNHCore.id("zinc_sulfide_reduction"))
+                .inputItems(dust, ZincSulfide, 1)
+                .inputFluids(Hydrogen.getFluid(2000))
+                .outputItems(dust, Zinc, 1)
+                .outputFluids(HydrogenSulfide.getFluid(1000))
+                .blastFurnaceTemp(1200)
+                .EUt(30).duration(100)
+                .save(provider);
+
+        GTRecipeTypes.SIFTER_RECIPES.recipeBuilder(CTNHCore.id("cyanide_tailings_sifting"))
+                .inputItems(dust, CYANIDE_TAILINGS, 4)
+                .outputItems(dust, Copper, 1)
                 .chancedOutput(dust, Lead, 1500, 500)
                 .chancedOutput(dust, Iron, 1200, 400)
                 .chancedOutput(dust, Gallium, 1200, 400)
@@ -49,71 +133,9 @@ public class GoldChain {
                 .EUt(30).duration(80)
                 .save(provider);
 
-        // 4-7. Gold alloy recipes
-        GTRecipeTypes.ALLOY_SMELTER_RECIPES.recipeBuilder(CTNHCore.id("gold_alloy1"))
-                .inputItems(dust, PreciousAlloy)
-                .inputItems(dust, Copper, 3)
-                .outputItems(ingot, GOLD_ALLOY, 4)
-                .EUt(30).duration(100)
-                .save(provider);
-
-        GTRecipeTypes.ALLOY_SMELTER_RECIPES.recipeBuilder(CTNHCore.id("gold_alloy2"))
-                .inputItems(ingot, PreciousAlloy)
-                .inputItems(dust, Copper, 3)
-                .outputItems(ingot, GOLD_ALLOY, 4)
-                .EUt(30).duration(100)
-                .save(provider);
-
-        GTRecipeTypes.ALLOY_SMELTER_RECIPES.recipeBuilder(CTNHCore.id("gold_alloy3"))
-                .inputItems(dust, PreciousAlloy)
-                .inputItems(ingot, Copper, 3)
-                .outputItems(ingot, GOLD_ALLOY, 4)
-                .EUt(30).duration(100)
-                .save(provider);
-
-        GTRecipeTypes.ALLOY_SMELTER_RECIPES.recipeBuilder(CTNHCore.id("gold_alloy4"))
-                .inputItems(ingot, PreciousAlloy)
-                .inputItems(ingot, Copper, 3)
-                .outputItems(ingot, GOLD_ALLOY, 4)
-                .EUt(30).duration(100)
-                .save(provider);
-
-        // 8. Gold leach dust
-        GTRecipeTypes.CHEMICAL_RECIPES.recipeBuilder(CTNHCore.id("gold_leach_dust"))
-                .inputItems(ingot, GOLD_ALLOY, 4)
-                .inputFluids(GTMaterials.NitricAcid.getFluid(1000))
-                .outputItems(dust, GOLD_LEACH, 4)
-                .outputFluids(GTMaterials.NitrogenDioxide.getFluid(1000))
-                .EUt(120).duration(80)
-                .save(provider);
-
-        // 9. Copper leach dust
-        GTRecipeTypes.CHEMICAL_RECIPES.recipeBuilder(CTNHCore.id("copper_leach_dust"))
-                .inputItems(dust, GOLD_LEACH, 4)
-                .inputFluids(GTMaterials.HydrochloricAcid.getFluid(1000))
-                .outputItems(dust, COPPER_LEACH, 4)
-                .outputFluids(CHLOROAURIC_ACID.getFluid(1000))
-                .EUt(120).duration(80)
-                .save(provider);
-
-        // 10. Chloroauric acid to gold
-        GTRecipeTypes.CHEMICAL_RECIPES.recipeBuilder(CTNHCore.id("chloroauricacid_to_gold"))
-                .inputFluids(CHLOROAURIC_ACID.getFluid(1000))
-                .notConsumable(dust, BauxiteProcessingMaterials.POTASSIUM_METABI_SULFITE)
-                .outputItems(dust, Gold, 2)
-                .outputFluids(GTMaterials.Water.getFluid(1000))
-                .outputFluids(GTMaterials.Chlorine.getFluid(1000))
-                .EUt(120).duration(100)
-                .save(provider);
-
-        // 11. Potassium metabisulfite
-        GTRecipeTypes.MIXER_RECIPES.recipeBuilder(CTNHCore.id("potassium_metabi_sulfite_dust"))
-                .circuitMeta(1)
-                .inputItems(dust, Potassium, 2)
-                .inputItems(dust, Sulfur, 2)
-                .inputFluids(GTMaterials.Oxygen.getFluid(5000))
-                .outputItems(dust, BauxiteProcessingMaterials.POTASSIUM_METABI_SULFITE, 9)
-                .EUt(30).duration(100)
-                .save(provider);
+        VanillaRecipeHelper.addSmeltingRecipe(provider, CTNHCore.id("precious_alloy_ingot_smelting"),
+                ChemicalHelper.get(ingot, PreciousAlloy),
+                ChemicalHelper.get(nugget, Gold, 3),
+                0.5f);
     }
 }
