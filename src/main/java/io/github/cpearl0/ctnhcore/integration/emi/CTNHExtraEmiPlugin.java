c57898a6fe43fdd550f5f3de6ed607a546d73302
lucky_block <1774432258@qq.com>
2026-08-21 00:31:28 +0800

    将despoil_loot的催化剂显示移到core

--- src/main/java/io/github/cpearl0/ctnhcore/integration/emi/CTNHExtraEmiPlugin.java
+++ src/main/java/io/github/cpearl0/ctnhcore/integration/emi/CTNHExtraEmiPlugin.java
@@ -1,9 +1,18 @@
 package io.github.cpearl0.ctnhcore.integration.emi;
 
+import com.gregtechceu.gtceu.api.item.IGTTool;
 import com.gregtechceu.gtceu.api.item.tool.GTToolType;
 import com.gregtechceu.gtceu.common.data.GTMaterialItems;
 
+import net.minecraft.world.item.EnchantedBookItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.item.enchantment.EnchantmentInstance;
+
+import com.github.elenterius.biomancy.init.ModEnchantments;
 import com.jesz.createdieselgenerators.CreateDieselGenerators;
+import com.moguang.ctnhbio.CTNHBio;
+import com.moguang.ctnhbio.api.item.tool.CBToolType;
 import com.tterrag.registrate.util.entry.ItemProviderEntry;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
@@ -11,8 +20,12 @@ import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.registry.EmiRecipes;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+
+import static com.github.elenterius.biomancy.init.ModItems.DESPOIL_SICKLE;
+import static com.moguang.ctnhbio.registry.CBMaterialItems.CB_TOOL_ITEMS;
 
 // This plugin is registered later than any other plugin
 public class CTNHExtraEmiPlugin implements EmiPlugin {
@@ -45,6 +58,24 @@ public class CTNHExtraEmiPlugin implements EmiPlugin {
                 .findFirst()
                 .ifPresent(emiRecipeCategory -> {
                     registry.addWorkstation(emiRecipeCategory, EmiIngredient.of(wireCutters));
+                });
+
+        ItemStack enchantedBook = EnchantedBookItem
+                .createForEnchantment(new EnchantmentInstance(ModEnchantments.DESPOIL.get(), 1));
+        var list = new ArrayList<EmiIngredient>();
+        for (ItemProviderEntry<IGTTool> entry : CB_TOOL_ITEMS.column(CBToolType.BONING_KNIFE).values()) {
+            if (entry == null) continue;
+            ItemStack stack = new ItemStack(entry.get());
+            list.add(EmiIngredient.of(Ingredient.of(stack)));
+        }
+
+        EmiRecipes.categories.stream()
+                .filter(c -> c.id.equals(CTNHBio.id("despoil_loot")))
+                .findFirst()
+                .ifPresent(category -> {
+                    registry.addWorkstation(category, EmiIngredient.of(Ingredient.of(DESPOIL_SICKLE.get())));
+                    registry.addWorkstation(category, EmiIngredient.of(Ingredient.of(enchantedBook)));
+                    registry.addWorkstation(category, EmiIngredient.of(list));
                 });
     }
 }
