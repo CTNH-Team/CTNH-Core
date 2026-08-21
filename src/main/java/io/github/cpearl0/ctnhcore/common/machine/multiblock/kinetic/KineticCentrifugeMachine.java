f0c4690de80633c1ced6d99fef15d5f1a5a1a00a
uraniniteresidue <2549847059@qq.com>
2026-08-22 00:33:29 +0800

    机械离心厂换结构

---
+++ src/main/java/io/github/cpearl0/ctnhcore/common/machine/multiblock/kinetic/KineticCentrifugeMachine.java
@@ -0,0 +1,93 @@
+package io.github.cpearl0.ctnhcore.common.machine.multiblock.kinetic;
+
+import com.gregtechceu.gtceu.api.machine.IMachineBlockEntity;
+import com.mo_guang.ctpp.common.machine.multiblock.KineticWorkableMultiblockMachine;
+import com.mo_guang.ctpp.dynamicPart.rotation.IContraptionMultiblock;
+import com.mo_guang.ctpp.dynamicPart.rotation.SimpleRotatingContraptionEntity;
+import lombok.Getter;
+import lombok.Setter;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.phys.Vec3;
+import tech.vixhentx.mcmod.ctnhlib.utils.MachineUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class KineticCentrifugeMachine extends KineticWorkableMultiblockMachine
+        implements IContraptionMultiblock<SimpleRotatingContraptionEntity> {
+
+    @Getter
+    @Setter
+    List<SimpleRotatingContraptionEntity> contraptionEntity = new ArrayList<>();
+
+    public KineticCentrifugeMachine(IMachineBlockEntity holder) {
+        super(holder);
+    }
+
+    @Override
+    public void onStructureFormed() {
+        super.onStructureFormed();
+        // assemble rotating entities using interface helper
+        createAndAttachRotatingEntities(MachineUtils.getOffset(this, 0, 0, 3), getContraptionRotationAxis());
+    }
+
+    @Override
+    public void onStructureInvalid() {
+        super.onStructureInvalid();
+        if (getLevel() != null && !getLevel().isClientSide) {
+            clearAndDisassembleRotatingEntities();
+        }
+    }
+
+    @Override
+    public void updateMachineSpeed() {
+        super.updateMachineSpeed();
+        if (!contraptionEntity.isEmpty() && isFormed) {
+            for (int i = 0 ; i < contraptionEntity.size() ; i++) {
+                SimpleRotatingContraptionEntity entity = contraptionEntity.get(i);
+                if (i == 0){
+                    entity.setRotationSpeedRPM(new Vec3(0, 1, 0), Math.min(speed, 128));
+                }else {
+                    entity.setRotationSpeedRPM(new Vec3(0, -1, 0), Math.min(speed, 128));
+                }
+            }
+        }
+    }
+
+    @Override
+    public void updateRotateBlocks(boolean active) {
+        super.updateRotateBlocks(active);
+        if (active) {
+            if (contraptionEntity != null)
+                for (int i = 0 ; i < contraptionEntity.size() ; i++) {
+                    SimpleRotatingContraptionEntity entity = contraptionEntity.get(i);
+                    if (i == 0){
+                        entity.setRotationSpeedRPM(new Vec3(0, 1, 0), Math.min(speed, 128));
+                    }else {
+                        entity.setRotationSpeedRPM(new Vec3(0, -1, 0), Math.min(speed, 128));
+                    }
+                }
+        }
+    }
+
+    public Map<Integer, SimpleRotatingContraptionEntity> assemble(BlockPos pivot) {
+        return assembleFromPattern(pivot, getContraptionRotationAxis());
+    }
+
+    private Direction.Axis getContraptionRotationAxis() {
+        return getFrontFacing().getAxis() == Direction.Axis.Z ? Direction.Axis.X : Direction.Axis.Z;
+    }
+
+    @Override
+    public BlockPos getAssemblyPivot() {
+        return MachineUtils.getOffset(this, 0, 0, 3);
+    }
+
+    @Override
+    public void onDebugAssembled() {
+        updateMachineSpeed();
+        updateRotateBlocks(getRecipeLogic().isWorking());
+    }
+}
--- src/main/java/io/github/cpearl0/ctnhcore/registry/machines/multiblock/Mechanical.java
+++ src/main/java/io/github/cpearl0/ctnhcore/registry/machines/multiblock/Mechanical.java
@@ -1,8 +1,7 @@
 package io.github.cpearl0.ctnhcore.registry.machines.multiblock;
 
-import io.github.cpearl0.ctnhcore.common.machine.multiblock.kinetic.KineticMixerMachine;
-import io.github.cpearl0.ctnhcore.data.CreateRecipeTypes;
-
+import com.ctnhlang.CN;
+import com.ctnhlang.EN;
 import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.data.RotationState;
 import com.gregtechceu.gtceu.api.machine.MultiblockMachineDefinition;
@@ -10,15 +9,7 @@ import com.gregtechceu.gtceu.api.pattern.FactoryBlockPattern;
 import com.gregtechceu.gtceu.api.pattern.MultiblockShapeInfo;
 import com.gregtechceu.gtceu.api.pattern.Predicates;
 import com.gregtechceu.gtceu.common.data.GTMaterials;
-
 import com.lowdragmc.lowdraglib.utils.BlockInfo;
-
-import net.minecraft.ChatFormatting;
-import net.minecraft.core.Direction;
-import net.minecraft.world.level.block.Blocks;
-
-import com.ctnhlang.CN;
-import com.ctnhlang.EN;
 import com.mo_guang.ctpp.CTPP;
 import com.mo_guang.ctpp.api.CTPPPartAbility;
 import com.mo_guang.ctpp.api.pattern.FactoryStaticBlockPattern;
@@ -28,10 +19,18 @@ import com.mo_guang.ctpp.util.CommonTooltips;
 import com.negodya1.vintageimprovements.VintageBlocks;
 import com.simibubi.create.AllBlocks;
 import com.simibubi.create.content.kinetics.base.DirectionalKineticBlock;
+import io.github.cpearl0.ctnhcore.common.machine.multiblock.kinetic.KineticCentrifugeMachine;
+import io.github.cpearl0.ctnhcore.common.machine.multiblock.kinetic.KineticMixerMachine;
+import io.github.cpearl0.ctnhcore.data.CreateRecipeTypes;
+import net.minecraft.ChatFormatting;
+import net.minecraft.core.Direction;
+import net.minecraft.world.level.block.Blocks;
 import tech.vixhentx.mcmod.ctnhlib.langprovider.Lang;
 
+import static com.gregtechceu.gtceu.common.data.GCYMBlocks.CASING_INDUSTRIAL_STEAM;
 import static com.gregtechceu.gtceu.common.data.GTBlocks.CASING_STEEL_GEARBOX;
 import static com.gregtechceu.gtceu.common.data.GTBlocks.CASING_TEMPERED_GLASS;
+import static com.jesz.createdieselgenerators.CDGBlocks.BULK_FERMENTER;
 import static com.simibubi.create.content.kinetics.base.HorizontalKineticBlock.HORIZONTAL_FACING;
 import static io.github.cpearl0.ctnhcore.registry.CTNHRegistration.REGISTRATE;
 
@@ -103,29 +102,38 @@ public class Mechanical {
                     GTCEu.id("block/multiblock/large_chemical_reactor"))
             .register();
     public final static MultiblockMachineDefinition MECHANICAL_CENTRIFUGE = REGISTRATE
-            .multiblock("mechanical_centrifuge", KineticWorkableMultiblockMachine::new)
+            .multiblock("mechanical_centrifuge", KineticCentrifugeMachine::new)
             .rotationState(RotationState.NON_Y_AXIS)
             .recipeType(CreateRecipeTypes.MECHANICAL_CENTRIFUGE_RECIPES)
             .appearanceBlock(AllBlocks.RAILWAY_CASING)
             .recipeModifier(CTPPRecipeModifiers.KINETIC_PARALLEL)
             .tooltips(CommonTooltips.KINETIC_OVERCLOCK.translate())
-            .pattern(definition -> FactoryBlockPattern.start()
-                    .aisle("AAAAA", "BCCCB", "B###B", "B###B", "B###B", "BBBBB", "AAAAA")
-                    .aisle("AAAAA", "CDDDC", "#####", "#####", "#####", "BF#FB", "AAAAA")
-                    .aisle("AAAAA", "CDDDC", "##G##", "##G##", "##G##", "B#H#B", "AAAAA")
-                    .aisle("AAAAA", "CDDDC", "#####", "#####", "#####", "BF#FB", "AAAAA")
-                    .aisle("AA@AA", "BCCCB", "B###B", "B###B", "B###B", "BBBBB", "AAAAA")
-                    .where("A", Predicates.blocks(AllBlocks.RAILWAY_CASING.get())
+            .pattern(definition -> FactoryStaticBlockPattern.start()
+                    .aisle("#############", "#############", "#####AAA#####", "#####AAA#####", "#####AAA#####", "#############")
+                    .aisle("#############", "#############", "#####AAA#####", "#####AAA#####", "#####AAA#####", "#####BBB#####")
+                    .aisle("#############", "#############", "#####AAA#####", "#####AAA#####", "#####AAA#####", "###BBCCCBB###")
+                    .aisle("####DDDDD####", "#############", "#############", "#####E#E#####", "#############", "##B##C#C##B##")
+                    .aisle("###DDDDDDD###", "#####DDD#####", "#############", "#####E#E#####", "#############", "##B##C#C##B##")
+                    .aisle("###DDDDDDD###", "####DDDDD####", "AAA#######AAA", "AAAEEFFFEEAAA", "AAA#######AAA", "#BCCCGGGCCCB#")
+                    .aisle("###DDDDDDD###", "####DDHDD####", "AAA###H###AAA", "AAA##FHF##AAA", "AAA###H###AAA", "#BC##GHG##CB#")
+                    .aisle("###DDDDDDD###", "####DDDDD####", "AAA#######AAA", "AAAEEFFFEEAAA", "AAA#######AAA", "#BCCCGGGCCCB#")
+                    .aisle("###DDDDDDD###", "#####DDD#####", "#############", "#####E#E#####", "#############", "##B##C#C##B##")
+                    .aisle("####DD@DD####", "#############", "#############", "#####E#E#####", "#############", "##B##C#C##B##")
+                    .aisle("#############", "#############", "#####AAA#####", "#####AAA#####", "#####AAA#####", "###BBCCCBB###")
+                    .aisle("#############", "#############", "#####AAA#####", "#####AAA#####", "#####AAA#####", "#####BBB#####")
+                    .aisle("#############", "#############", "#####AAA#####", "#####AAA#####", "#####AAA#####", "#############")
+                    .where("D", Predicates.blocks(AllBlocks.RAILWAY_CASING.get())
                             .or(Predicates.autoAbilities(definition.getRecipeTypes()))
-                            .or(Predicates.abilities(CTPPPartAbility.INPUT_KINETIC))
+                            .or(Predicates.abilities(CTPPPartAbility.INPUT_KINETIC).setMinGlobalLimited(1))
                             .or(Predicates.abilities(CTPPPartAbility.MECHANICAL_UPGRADE).setExactLimit(1)))
-                    .where("B", Predicates.blocks(AllBlocks.METAL_GIRDER.get()))
-                    .where("C", Predicates.blocks(AllBlocks.BRASS_CASING.get()))
+                    .where("B", Predicates.blocks(CASING_INDUSTRIAL_STEAM.get()), false, 0)
+                    .where("C", Predicates.blocks(AllBlocks.METAL_GIRDER.get()), false,0)
+                    .where("G", Predicates.blocks(AllBlocks.RAILWAY_CASING.get()), false, 0)
+                    .where("E", Predicates.blocks(AllBlocks.METAL_GIRDER.get()), false,1)
+                    .where("F", Predicates.blocks(AllBlocks.RAILWAY_CASING.get()), false, 1)
+                    .where("A", Predicates.blocks(BULK_FERMENTER.get()), false, 1)
+                    .where("H", Predicates.blocks(CASING_STEEL_GEARBOX.get()))
                     .where("#", Predicates.any())
-                    .where("D", Predicates.blocks(AllBlocks.BASIN.get()))
-                    .where("F", Predicates.blocks(AllBlocks.BRASS_ENCASED_COGWHEEL.get()))
-                    .where("G", Predicates.blocks(VintageBlocks.CENTRIFUGE.get()))
-                    .where("H", Predicates.blocks(AllBlocks.BRASS_ENCASED_LARGE_COGWHEEL.get()))
                     .where("@", Predicates.controller(Predicates.blocks(definition.get())))
                     .build())
             .workableCasingModel(CTPP.id("block/create/railway_casing"),
