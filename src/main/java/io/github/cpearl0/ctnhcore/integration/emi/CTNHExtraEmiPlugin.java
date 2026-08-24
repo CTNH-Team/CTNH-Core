2bf2998502d633953c78ddfcdf668c46ea72ac73
lucky_block <1774432258@qq.com>
2026-08-24 18:00:15 +0800

    移除对moonlight lib的修复

--- src/main/java/io/github/cpearl0/ctnhcore/integration/emi/CTNHExtraEmiPlugin.java
+++ src/main/java/io/github/cpearl0/ctnhcore/integration/emi/CTNHExtraEmiPlugin.java
@@ -1,12 +1,13 @@
 package io.github.cpearl0.ctnhcore.integration.emi;
 
+import com.gregtechceu.gtceu.GTCEu;
 import com.gregtechceu.gtceu.api.item.IGTTool;
 import com.gregtechceu.gtceu.api.item.tool.GTToolType;
 import com.gregtechceu.gtceu.common.data.GTMaterialItems;
 
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.EnchantedBookItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.crafting.Ingredient;
 import net.minecraft.world.item.enchantment.EnchantmentInstance;
 
 import com.github.elenterius.biomancy.init.ModEnchantments;
@@ -16,13 +17,14 @@ import com.moguang.ctnhbio.api.item.tool.CBToolType;
 import com.tterrag.registrate.util.entry.ItemProviderEntry;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
+import dev.emi.emi.api.recipe.EmiRecipeCategory;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.registry.EmiRecipes;
+import tech.luckyblock.mcmod.ctnhenergy.registry.CEItems;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
+import java.util.function.Consumer;
 
 import static com.github.elenterius.biomancy.init.ModItems.DESPOIL_SICKLE;
 import static com.moguang.ctnhbio.registry.CBMaterialItems.CB_TOOL_ITEMS;
@@ -30,8 +32,14 @@ import static com.moguang.ctnhbio.registry.CBMaterialItems.CB_TOOL_ITEMS;
 // This plugin is registered later than any other plugin
 public class CTNHExtraEmiPlugin implements EmiPlugin {
 
+    private static Map<ResourceLocation, EmiRecipeCategory> categoryMap = new HashMap<>();
+
     @Override
     public void register(EmiRegistry registry) {
+        for (var category : EmiRecipes.categories) {
+            categoryMap.put(category.id, category);
+        }
+
         List<EmiStack> hammers = GTMaterialItems.TOOL_ITEMS.column(GTToolType.HARD_HAMMER)
                 .values().stream()
                 .filter(Objects::nonNull)
@@ -39,12 +47,9 @@ public class CTNHExtraEmiPlugin implements EmiPlugin {
                 .map(EmiStack::of)
                 .toList();
 
-        EmiRecipes.categories.stream()
-                .filter(c -> c.getId().equals(CreateDieselGenerators.rl("hammering")))
-                .findFirst()
-                .ifPresent(emiRecipeCategory -> {
-                    registry.addWorkstation(emiRecipeCategory, EmiIngredient.of(hammers));
-                });
+        handleCategory(CreateDieselGenerators.rl("hammering"), emiRecipeCategory -> {
+            registry.addWorkstation(emiRecipeCategory, EmiIngredient.of(hammers));
+        });
 
         List<EmiStack> wireCutters = GTMaterialItems.TOOL_ITEMS.column(GTToolType.WIRE_CUTTER)
                 .values().stream()
@@ -53,29 +58,34 @@ public class CTNHExtraEmiPlugin implements EmiPlugin {
                 .map(EmiStack::of)
                 .toList();
 
-        EmiRecipes.categories.stream()
-                .filter(c -> c.getId().equals(CreateDieselGenerators.rl("wire_cutting")))
-                .findFirst()
-                .ifPresent(emiRecipeCategory -> {
-                    registry.addWorkstation(emiRecipeCategory, EmiIngredient.of(wireCutters));
-                });
+        handleCategory(CreateDieselGenerators.rl("wire_cutting"), emiRecipeCategory -> {
+            registry.addWorkstation(emiRecipeCategory, EmiIngredient.of(wireCutters));
+        });
 
         ItemStack enchantedBook = EnchantedBookItem
                 .createForEnchantment(new EnchantmentInstance(ModEnchantments.DESPOIL.get(), 1));
-        var list = new ArrayList<EmiIngredient>();
+        var boningKnifes = new ArrayList<EmiIngredient>();
         for (ItemProviderEntry<IGTTool> entry : CB_TOOL_ITEMS.column(CBToolType.BONING_KNIFE).values()) {
             if (entry == null) continue;
             ItemStack stack = new ItemStack(entry.get());
-            list.add(EmiIngredient.of(Ingredient.of(stack)));
+            boningKnifes.add(EmiStack.of(stack));
         }
 
-        EmiRecipes.categories.stream()
-                .filter(c -> c.id.equals(CTNHBio.id("despoil_loot")))
-                .findFirst()
-                .ifPresent(category -> {
-                    registry.addWorkstation(category, EmiIngredient.of(Ingredient.of(DESPOIL_SICKLE.get())));
-                    registry.addWorkstation(category, EmiIngredient.of(Ingredient.of(enchantedBook)));
-                    registry.addWorkstation(category, EmiIngredient.of(list));
-                });
+        handleCategory(CTNHBio.id("despoil_loot"), category -> {
+            registry.addWorkstation(category, EmiStack.of(DESPOIL_SICKLE.get()));
+            registry.addWorkstation(category, EmiStack.of(enchantedBook));
+            registry.addWorkstation(category, EmiIngredient.of(boningKnifes));
+        });
+
+        handleCategory(GTCEu.id("programmed_circuit"), category -> {
+            registry.addWorkstation(category, EmiStack.of(CEItems.PROGRAMMED_CIRCUIT_CARD.get()));
+        });
+    }
+
+    private static void handleCategory(ResourceLocation id, Consumer<EmiRecipeCategory> consumer) {
+        var category = categoryMap.get(id);
+        if (category != null) {
+            consumer.accept(category);
+        }
     }
 }
--- src/main/java/io/github/cpearl0/ctnhcore/mixin/ars_nouveau/TerminalSyncManagerMixin.java
+++ src/main/java/io/github/cpearl0/ctnhcore/mixin/ars_nouveau/TerminalSyncManagerMixin.java
@@ -2,13 +2,15 @@ package io.github.cpearl0.ctnhcore.mixin.ars_nouveau;
 
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.world.item.ItemStack;
+
+import com.hollingsworth.arsnouveau.client.container.TerminalSyncManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 /** Keeps arbitrary item NBT in Ars Nouveau's Bookwyrm terminal sync. */
-@Mixin(targets = "com.hollingsworth.arsnouveau.client.container.TerminalSyncManager", remap = false)
+@Mixin(value = TerminalSyncManager.class, remap = false)
 public class TerminalSyncManagerMixin {
 
     @Inject(method = "getSyncTag", at = @At("RETURN"), cancellable = true)
