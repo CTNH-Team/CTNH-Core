3d1204a65835c4c3931d80b00389757c3e04ee5d
uraniniteresidue <2549847059@qq.com>
2026-08-29 00:36:28 +0800

    添加精密系列电路配方，更改HV阶段晶圆蚀刻流程

--- src/main/java/io/github/cpearl0/ctnhcore/data/recipe/CTNHCoreRecipeAddition.java
+++ src/main/java/io/github/cpearl0/ctnhcore/data/recipe/CTNHCoreRecipeAddition.java
@@ -18,6 +18,7 @@ import io.github.cpearl0.ctnhcore.data.recipe.migrated.GtceuScriptRecipes;
 import io.github.cpearl0.ctnhcore.data.recipe.modmodify.EIORecipes;
 import io.github.cpearl0.ctnhcore.data.recipe.modmodify.omnicells.QuantumOmniRecipes;
 import io.github.cpearl0.ctnhcore.data.recipe.multiblock.*;
+import io.github.cpearl0.ctnhcore.data.recipe.WaferRecipes;
 import io.github.cpearl0.ctnhcore.data.recipe.wood.WoodMachineRecipes;
 
 import net.minecraft.data.recipes.FinishedRecipe;
@@ -53,6 +54,7 @@ public class CTNHCoreRecipeAddition {
 
         HyperRotorRecipes.registerAll(provider);
         OrdinaryRecipes.init(provider);
+        WaferRecipes.init(provider);
         AssemblyLineRecipes.init(provider);
 
         // 兼容 mod 配方
